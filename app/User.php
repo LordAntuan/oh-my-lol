2c293a82ae9662d9e22a03185fec7d329a0aa2ca
Antonio Luján <bi0snake@hotmail.com>
2020-08-09 20:54:41 +0200

    [feat] Modified User model to use SoftDeletes and modified users migration to have deleted_at

--- app/User.php
+++ app/User.php
@@ -3,20 +3,21 @@
 namespace App;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, SoftDeletes;
 
     /**
-     * The attributes that are mass assignable.
+     * The attributes that aren't mass assignable.
      *
      * @var array
      */
-    protected $fillable = [
-        'name', 'email', 'password',
+    protected $guarded = [
+        'created_at', 'updated_at', 'deleted_at'
     ];
 
     /**
